5427f8516dbe6a2513b265d5eda48fbf915b743a
Anastasiia Voronkova <138679587+delichon-urbica@users.noreply.github.com>
2023-11-22 16:09:10 +0100

    Add files via upload

---
+++ minitalk/minitalk.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avoronko <avoronko@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 15:09:31 by avoronko          #+#    #+#             */
+/*   Updated: 2023/09/29 13:13:09 by avoronko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# define _GNU_SOURCE
+
+# include "includes/libft/libft.h"
+# include "includes/ft_printf/ft_printf.h"
+# include <signal.h>
+# include <stddef.h> 
+# include <unistd.h>
+# include <sys/types.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+#endif
